216df3a7b34cbba2e89b9b6756370795e095c61e
Vanderson <vanderson.koringa299@gmail.com>
2025-12-15 18:32:24 -0300

    Starbucks landing page

---
+++ src/js/script.js
@@ -0,0 +1,48 @@
+const mobileBtn = document.querySelector(".btn-mobile");
+const navLinks = document.querySelector("#nav-links");
+const icon = document.querySelector(".btn-mobile i");
+const header = document.getElementById("header");
+
+window.addEventListener("scroll" , ()=>{
+
+    if(window.scrollY > 0){
+        header.style.boxShadow = "0 4px 6px rgba(0 , 0 , 0 , 0.1)"
+    } else {
+        header.style.boxShadow = "none"
+    }
+
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+    });
+
+
+});
+
+navLinks.querySelectorAll("a").forEach((link , index) => {
+    link.addEventListener("click" , ()=>{
+        navLinks.classList.remove("show");
+        
+        if(icon.classList.contains("fa-bars")){
+            icon.classList.remove("fa-bars");
+            icon.classList.add("fa-times");
+        } else {
+            icon.classList.remove("fa-times");
+            icon.classList.add("fa-bars");
+        }
+    });
+});
+
+mobileBtn.addEventListener("click",()=>{
+    navLinks.classList.toggle("show");
+    icon.classList.toggle("fa-times");
+    icon.classList.toggle("fa-bars");
+
+
+
+});
+
+
+
+// Scroll suave com offset personalizado
+
